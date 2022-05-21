6e76761cc912dec83656194bbd8cfc27d1a6b242
rkrana78 <rashedul.karim7858@gmail.com>
2022-05-21 23:42:35 +0600

    responsive issu update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,56 +7,63 @@ import './Navbar.css'
 const Navbar = () => {
     const [navbarState, setNavbarState] = useState(false)
     return (
-        <div className='nav-container'>
-           
-                <div className="brand">
-                    <div className="container">
-                        <img src={logo} alt='' />Travel BD
+        <>
+            <nav className='nav-container'>
+            
+                    <div className="brand">
+                        <div className="container">
+                            <img src={logo} alt='' />Travel BD
+                        </div>
+                        <div className="toggle">
+                            {navbarState ? (
+                                <VscChromeClose onClick={() => setNavbarState(false)} />
+                            ) : (
+                                <GiHamburgerMenu onClick={() => setNavbarState(!navbarState)} />
+                            )}
+                        </div>
                     </div>
-                    <div className="toggle">
-                        {navbarState ? (
-                            <VscChromeClose onClick={() => setNavbarState(false)} />
-                        ) : (
-                            <GiHamburgerMenu onClick={() => setNavbarState(true)} />
-                        )}
-                    </div>
-                </div>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#recommend">Place</a></li>
-                    <li><a href="#testimonials">Testimonials</a></li>
+                    <div className= {
+                        navbarState ? 'menu-link mobile-menu-link' : 'menu-link'
+                    }>
+                        <ul>
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#recommend">Place</a></li>
+                            <li><a href="#testimonials">Testimonials</a></li>
 
-                </ul>
-                <button>Connect</button>
-           
-            <div state={navbarState} className='navbar-res'>
-                <ul>
-                    <li>
-                        <a href="#home" onClick={() => setNavbarState(false)}>
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#services" onClick={() => setNavbarState(false)}>
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#recommend" onClick={() => setNavbarState(false)}>
-                            Places
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#testimonials" onClick={() => setNavbarState(false)}>
-                            Testimonials
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+                        </ul>
+                    </div>
+                    <button>Connect</button>
+            
+                {/* <div className={navbarState ? '': 'navbar-res'}>
+                    <ul>
+                        <li>
+                            <a href="#home" onClick={() => setNavbarState(false)}>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#services" onClick={() => setNavbarState(false)}>
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#recommend" onClick={() => setNavbarState(false)}>
+                                Places
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#testimonials" onClick={() => setNavbarState(false)}>
+                                Testimonials
+                            </a>
+                        </li>
+                    </ul>
+                </div> */}
+            </nav>
+        </>
     );
 };
 
 export default Navbar;
+
 
